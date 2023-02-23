c559bc49ef084e10f0bd266f141e6c29bff2ba57
lich <lich123good@gmail.com>
2023-02-23 22:53:03 +0800

    将每个格中的数字显示在格中央，并且添加储存每个格数据的资源，并且接收键盘的上下左右

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,7 @@ use bevy::prelude::*;
 pub static WINDOW_WIDTH: f32 = 800.0;
 pub static WINDOW_HEIGHT: f32 = 600.0;
 pub static CELL_SPACE: f32 = 6.0;
-pub static CELL_SIDE_NUM: i32 = 4;
+pub static CELL_SIDE_NUM: u32 = 4;
 pub static COLOR_BACKGROUND: Color = Color::rgb(187.0 / 255.0, 173.0 / 255.0, 160.0 / 255.0);
 pub static COLOR_CELL_NULL: Color = Color::rgb(204.0 / 255.0, 192.0 / 255.0, 178.0 / 255.0);
 pub static COLOR_CELL_2: Color = Color::rgb(238.0 / 255.0, 228.0 / 255.0, 218.0 / 255.0);
@@ -18,3 +18,37 @@ pub static COLOR_CELL_256: Color = Color::rgb(239.0 / 255.0, 203.0 / 255.0, 97.0
 pub static COLOR_CELL_512: Color = Color::rgb(239.0 / 255.0, 193.0 / 255.0, 45.0 / 255.0);
 pub static COLOR_CELL_1024: Color = Color::rgb(239.0 / 255.0, 197.0 / 255.0, 63.0 / 255.0);
 pub static COLOR_CELL_2048: Color = Color::rgb(238.0 / 255.0, 194.0 / 255.0, 46.0 / 255.0);
+
+pub static COLOR_BROWN: Color = Color::rgb(120.0 / 255.0, 110.0 / 255.0, 100.0 / 255.0);
+pub static COLOR_WHITE: Color = Color::rgb(245.0 / 255.0, 250.0 / 255.0, 240.0 / 255.0);
+
+pub enum MOVE_DIRECTION {
+	NONE,
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+}
+
+#[derive(Resource)]
+pub struct CELL_VALUE_SAVE {
+	pub(crate) valueSave: Vec<Vec<u32>>
+}
+
+pub fn Move_Value(direction: MOVE_DIRECTION, saveValue: &mut Vec<Vec<u32>>) {
+
+	match direction {
+		MOVE_DIRECTION::NONE => return,
+		_ => {
+			for i in 0..saveValue.len() {
+				for j in 0..saveValue[i].len() {
+					saveValue[i][j] += 1;
+					print!("{} ", saveValue[i][j]);
+				}
+				println!("");
+			}
+		}
+	}
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ fn main() {
 		}))
 		.insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
 		.add_startup_system(setup)
+		.add_system(keyboard_input)
 		.run();
 }
 
@@ -52,6 +53,16 @@ fn setup(
 	mut meshes: ResMut<Assets<Mesh>>,
 	mut materials: ResMut<Assets<ColorMaterial>>
 ) {
+
+	let mut cell_value_save_temp: Vec<Vec<u32>> = Vec::new();
+	for i in 0..CELL_SIDE_NUM {
+		let mut cell_value_save_temp_row: Vec<u32> = Vec::new();
+		for j in 0..CELL_SIDE_NUM {
+			cell_value_save_temp_row.push(i * CELL_SIDE_NUM + j + 1);
+		}
+		cell_value_save_temp.push(cell_value_save_temp_row);
+	}
+	commands.insert_resource(CELL_VALUE_SAVE{valueSave: cell_value_save_temp});
 	commands.spawn(Camera2dBundle::default());
 
 	let side_length: f32 = (WINDOW_HEIGHT - CELL_SPACE * (CELL_SIDE_NUM as f32 + 1.0)) / CELL_SIDE_NUM as f32;
@@ -66,26 +77,46 @@ fn setup(
 		..default()
 	});
 
+	let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+	let text_style = TextStyle {
+		font,
+		font_size: side_length / 2.0,
+		color: COLOR_BROWN,
+	};
+	let box_size = Vec2::new(side_length, side_length);
+
 	for i in 0..CELL_SIDE_NUM {
 		for j in 0..CELL_SIDE_NUM {
 			commands.spawn(MaterialMesh2dBundle {
 				mesh: meshes.add(shape::Box::new(side_length, side_length, 0.0).into()).into(),
 				material: materials.add(ColorMaterial::from(COLOR_CELL_NULL)),
-				transform: Transform::from_xyz(x_offset + (j as f32) * (side_length + CELL_SPACE), y_offset - (i as f32) * (side_length + CELL_SPACE), 0.0),
+				transform: Transform::from_xyz(
+					x_offset + (j as f32) * (side_length + CELL_SPACE),
+					y_offset - (i as f32) * (side_length + CELL_SPACE),
+					0.0),
+				..default()
+			});
+
+			commands.spawn(Text2dBundle {
+				text: Text::from_section("2048", text_style.clone()).with_alignment(TextAlignment::CENTER),
+				text_2d_bounds: Text2dBounds {
+					// Wrap text in the rectangle
+					size: box_size,
+				},
+				// We align text to the top-left, so this transform is the top-left corner of our text. The
+				// box is centered at box_position, so it is necessary to move by half of the box size to
+				// keep the text in the box.
+				transform: Transform::from_xyz(
+					x_offset + (j as f32) * (side_length + CELL_SPACE),
+					y_offset - (i as f32) * (side_length + CELL_SPACE),
+					1.0),
 				..default()
 			});
 		}
 	}
 
-	let font = asset_server.load("fonts/FiraSans-Bold.ttf");
-	let text_style = TextStyle {
-		font,
-		font_size: 50.0,
-		color: Color::BLACK,
-	};
-	let box_size = Vec2::new(side_length, side_length);
 	commands.spawn(Text2dBundle {
-		text: Text::from_section("SCORE:", text_style),
+		text: Text::from_section("SCORE:", text_style.clone()),
 		text_2d_bounds: Text2dBounds {
 			// Wrap text in the rectangle
 			size: box_size,
@@ -99,7 +130,26 @@ fn setup(
 	});
 }
 
+fn keyboard_input(
+	keyboard_input: Res<Input<KeyCode>>,
+	mut cell_Value_Save: ResMut<CELL_VALUE_SAVE>
+) {
+	let mut moved = MOVE_DIRECTION::NONE;
+	if keyboard_input.just_pressed(KeyCode::Up) {
+		moved = MOVE_DIRECTION::UP;
+	}
+	if keyboard_input.just_pressed(KeyCode::Down) {
+		moved = MOVE_DIRECTION::DOWN;
+	}
+	if keyboard_input.just_pressed(KeyCode::Right) {
+		moved = MOVE_DIRECTION::RIGHT;
+	}
+	if keyboard_input.just_pressed(KeyCode::Left) {
+		moved = MOVE_DIRECTION::LEFT;
+	}
 
+	Move_Value(moved, &mut cell_Value_Save.valueSave);
+}
 
 
 
